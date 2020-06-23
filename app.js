c3edd819a586af74d8792eb2d301d44e3df08030
jogglez29 <38192813@uaz.edu.mx>
2020-06-23 18:31:18 -0500

    Boostrap e inicio página

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//bootstrap
+app.use('/css',express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js',express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js',express.static(__dirname + '/node_modules/popper.js/dist'));
+app.use('/js',express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
