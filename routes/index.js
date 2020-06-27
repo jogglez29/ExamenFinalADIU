be518e6d27b0aa68cb0127d8d4d8b1147963c92a
jogglez29 <38192813@uaz.edu.mx>
2020-06-26 19:29:46 -0500

    Acerca de

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { page: 'home' });
 });
 
 /* Entramos al acerca de. */
 router.get('/AcercaDe', function(req, res, next) {
-  res.render('AcercaDe', { title: 'Acerca De' });
+  res.render('AcercaDe', { page: 'Acerca De' });
 });
 
 module.exports = router;
